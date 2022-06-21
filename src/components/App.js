28754211155b2606b6c17afc8492b085dc2ea169
brody.sanford@gmail.com <brodysmac@brodens-mbp.lan>
2022-06-21 16:00:53 -0400

    added state for games variable and added initial GET fetch

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import GameContainer from "./GameContainer"
 function App() {
   return (
     <>
+    <h1>Hi!</h1>
     <Header />
     <GameContainer />
     </>
--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import GameList from './GameList'
 import GameSearch from './GameSearch'
 import NewGameForm from './NewGameForm'
 
 function GameContainer() {
+
+  const [games, setGames] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/games')
+    .then(resp => resp.json())
+    .then(data => setGames(data))
+  }, [])
+
+  console.log(games)
+
     return (
       <>
       <NewGameForm />
