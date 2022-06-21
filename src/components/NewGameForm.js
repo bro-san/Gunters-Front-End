f58b56d19a96a388a4b22291e1edba2b4b4f1f6a
justin.hobbs <justinhobbs@fastmail.com>
2022-06-21 15:40:47 -0500

    initial form creation

--- src/components/NewGameForm.js
+++ src/components/NewGameForm.js
@@ -1,11 +1,66 @@
-import React from 'react'
+import React from "react";
 
-function NewGameForm() {
-    return (
-      <>
-     
-      </>
-    );
-  }
-  
-  export default NewGameForm;
+function NewGameForm({ onAddGame }) {
+   const [name, setName] = useState("");
+   const [image, setImage] = useState("");
+   const [likes, setLikes] = useState("");
+   const [description, setDescription] = useState("");
+
+   function handleSubmit(e) {
+      e.preventDefault();
+      fetch("http://localhost:6001/plants", {
+         method: "POST",
+         headers: {
+            "Content-Type": "application/json",
+         },
+         body: JSON.stringify({
+            name: name,
+            image: image,
+            likes: likes,
+            description: description;
+         }),
+      })
+         .then((r) => r.json())
+         .then((newGame) => onAddGame(newGame));
+   }
+
+   return (
+      <div className='new-game-form'>
+         <h2>New Game</h2>
+         <form onSubmit={handleSubmit}>
+            <input
+               type='text'
+               name='name'
+               placeholder='Game name'
+               value={name}
+               onChange={(e) => setName(e.target.value)}
+            />
+            <input
+               type='text'
+               name='image'
+               placeholder='Image URL'
+               value={image}
+               onChange={(e) => setImage(e.target.value)}
+            />
+            <input
+               type='number'
+               name='likes'
+               step='1'
+               placeholder='Likes'
+               value={likes}
+               onChange={(e) => setLikes(parseFloat(e.target.value))}
+            />
+            <input
+               type='text'
+               name='description'
+               placeholder='Game description'
+               value={description}
+               onChange={(e) => setDescription(e.target.value)}
+            />
+            <button type='submit'>Add Game</button>
+         </form>
+      </div>
+   );
+}
+
+export default NewGameForm;
