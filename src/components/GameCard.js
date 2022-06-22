15451d765fa27051866c90793b518c1a10cd220a
justin.hobbs <justinhobbs@fastmail.com>
2022-06-22 14:45:16 -0500

    started game details page

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,22 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-function GameCard({game}) {
-
-    const {name, image, description, likes } = game
-    return (
-        <li className='card'>
-            
-            <div>
-                <h4>{name}</h4>
-                <img src={image} alt={name} />
-            </div>
-            <div>
-                <p>{description}</p>
-                <button>Likes: {likes}</button>
-            </div>
-        </li>
-
-    );
+function GameCard({ game }) {
+   const { name, image, description, likes, id } = game;
+   return (
+      <li className='card'>
+         <div>
+            <h4>{name}</h4>
+            <img src={image} alt={name} />
+         </div>
+         <div>
+            <p>{description}</p>
+            <Link to={`/games/${id}/details`} className='button'>
+               Details: {id}
+            </Link>
+            <br></br>
+            <br></br>
+            <button>Likes: {likes}</button>
+         </div>
+      </li>
+   );
 }
 
 export default GameCard;
